fb7f6e58cae0947522194b6113bc1c969a5d5b17
jowpurcinelli <jppurcinelli@gmail.com>
2020-10-10 20:07:33 -0300

    mainPages

---
+++ memberside/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Routes 
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
